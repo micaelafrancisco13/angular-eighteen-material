4328aea92b23ae0f81b7c3c883a041a690a86127
mifr <micaela.francisco@finastra.com>
2025-07-28 21:40:57 +0800

    temp-from-local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatFormField, MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {MatAnchor, MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatFormField, MatFormFieldModule, MatInputModule, MatLabel, MatInput, MatButton, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
